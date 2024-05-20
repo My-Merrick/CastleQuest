e071bdf1fcaf385ad44e194bf74c38390d8120a1
Dennis Merrick <bestdennisyeboah@gmail.com>
2024-05-20 21:36:31 +0000

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      {" "}
+      <h1 className="text-3xl font-bold underline text-slate-100 p-5 bg-slate-800 align-middle">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
